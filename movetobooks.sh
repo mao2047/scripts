1e48657a704379e4fea203fd02078cc9f88536e1
mao2047 <hel32ryuu@gmail.com>
2020-11-30 16:45:31 -0600

    some debbuging lol

--- movetobooks.sh
+++ movetobooks.sh
@@ -12,16 +12,16 @@ file1=$(fzf); echo $file1; read "?Introduce nuevo nombre: " file2; tmp=${file1:r
 	print "Mover a: \n f: filosofía | s: sociología | a: antropologia | t: tecnología\n c: ciencias  | i: psicología | l: literatura   | n: religión\n e: pedagogía | g: guias | *** cualquier otra tecla para salir ***"; \
 	read "? > " a; file1=(./$file1); file2=($file2.$ext); \
 	case $a in \
-		f) mv $file1 $HOME/Books/filosofia/$file2 ;; \
-		s) mv $file1 $HOME/Books/sociologia/$file2 ;; \
-		a) mv $file1 $HOME/Books/antropologia/$file2 ;; \
-		t) mv $file1 $HOME/Books/tecnologia/$file2 ;; \
-		c) mv $file1 $HOME/Books/ciencias/$file2 ;; \
-		g) mv $file1 $HOME/Books/guias/$file2 ;; \
-		i) mv $file1 $HOME/Books/psicologia/$file2 ;; \
-		l) mv $file1 $HOME/Books/literatura/$file2 ;; \
-		n) mv $file1 $HOME/Books/religion/$file2 ;; \
-		e) mv $file1 $HOME/Books/pedagogia/$file2 ;; \
+		f) mv ./$file1 $HOME/Books/filosofia/$file2.$ext ;; \
+		s) mv ./$file1 $HOME/Books/sociologia/$file2.$ext ;; \
+		a) mv ./$file1 $HOME/Books/antropologia/$file2.$ext ;; \
+		t) mv ./$file1 $HOME/Books/tecnologia/$file2.$ext ;; \
+		c) mv ./$file1 $HOME/Books/ciencias/$file2.$ext ;; \
+		g) mv ./$file1 $HOME/Books/guias/$file2.$ext ;; \
+		i) mv ./$file1 $HOME/Books/psicologia/$file2.$ext ;; \
+		l) mv ./$file1 $HOME/Books/literatura/$file2.$ext ;; \
+		n) mv ./$file1 $HOME/Books/religion/$file2.$ext ;; \
+		e) mv ./$file1 $HOME/Books/pedagogia/$file2.$ext ;; \
 		*) echo "bye"  ;; 
 	esac
 read "?x para  salir > " n; case $n in \
