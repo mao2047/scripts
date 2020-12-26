e6f2054d0f21d1d6276450d647948bca262d1071
mao2047 <hel32ryuu@gmail.com>
2020-12-25 19:21:15 -0600

    bar, new improvements

--- calcursesync.sh
+++ calcursesync.sh
@@ -1,3 +1,3 @@
 #! /bin/zsh
 #sincronizacion rudimentaria de calcurse
-calcurse && echo "Saving to dropbox" && cp -r ~/.local/share/calcurse/* ~/dropbox/calcurse && echo "done"
+calcurse && echo "Saving to dropbox" && cp -r ~/.local/share/calcurse/* ~/dp/calcurse && echo "done"
